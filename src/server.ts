c667747f4bb8d6baabc96df1d04e8aeaf88ea2c0
Utilisateur <lorent.max@gmail.com>
2022-04-15 12:05:20 +0200

    add graphQL

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,36 @@
-console.log('your code goes here'); // eslint-disable-line no-console
+import mysql from 'mysql2';
+import { graphql, buildSchema } from 'graphql';
+
+// ------------------- for check connection change host, user, password ------------------ //
+
+const con = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'admin',
+  password: 'Pa$$W0rd',
+});
+
+con.connect((err: any) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The rootValue provides a resolver function for each API endpoint
+const rootValue = {
+  hello: () => 'Hello world!',
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql({
+  schema,
+  source: '{ hello }',
+  rootValue,
+}).then((response) => {
+  console.log(response);
+});
