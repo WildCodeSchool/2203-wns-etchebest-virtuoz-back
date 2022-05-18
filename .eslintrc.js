e193b9cd8e2c47a5daf23185785f4b6845ec65b4
mosindo <mosindo.mbilinyama@hotmail.fr>
2022-05-18 11:20:46 +0200

    add last changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
     project: './tsconfig.json',
-    tsconfigRootDir: './',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
